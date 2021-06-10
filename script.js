c2e1dbcdcedc2a99e0dcb5b4c954b3b8dc188c66
Elvis <72048969+ElvisS10@users.noreply.github.com>
2021-06-10 01:01:25 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,87 +1,93 @@
-
     window.onload = () => {
-        const button = document.querySelector('button[data-action="change"]');
-        button.innerText = 'NEXT';
+      const button = document.querySelector('button[data-action="change"]');
+      button.innerText = "NEXT";
 
-        let places = staticLoadPlaces();
-        renderPlaces(places);
+      let places = staticLoadPlaces();
+      renderPlaces(places);
     };
 
-function staticLoadPlaces() {
-    return [
+    function staticLoadPlaces() {
+      return [
         {
-            name: 'Hamburguesa',
-            location: {
-                // decomment the following and add coordinates:
-                lat: 16.939544,
-                lng: -97.775664,
-            },
-        },
-    ];
+          name: "Hamburguesa",
+          location: {
+            // decomment the following and add coordinates:
+            lat: 16.939544,
+            lng: -97.775664,
+          }
+        }
+      ];
     }
     var models = [
-        {
-            url: 'https://cdn.glitch.com/05e1a300-5fe8-4c3d-aa8e-bc069bcb2db8%2FHamburguesa.glb?v=1623293425699',
-            scale: '6 6 6',
-            info: 'Hamburguesa, con la mejor carne, Precio: $60',
-            rotation: '0 -80 60',
-            
-        },
-        {
-            url: 'https://cdn.glitch.com/05e1a300-5fe8-4c3d-aa8e-bc069bcb2db8%2FPizza.glb?v=1623293471899',
-            scale: '30 30 30',
-            rotation: '0 180 0',
-            info: 'Pizza, Hawallana relleno de queso, Precio: $20',
-        },
-        {
-            url: 'https://cdn.glitch.com/05e1a300-5fe8-4c3d-aa8e-bc069bcb2db8%2Fcombo.glb?v=1623293390010',
-            scale: '3 3 3',
-            rotation: '250 100 -100',
-            info: 'Combo de Hamburguesa, incluye refresco, Precio: $120',
-        },
+      {
+        url:
+          "https://cdn.glitch.com/05e1a300-5fe8-4c3d-aa8e-bc069bcb2db8%2FHamburguesa.glb?v=1623293425699",
+        scale: "6 6 6",
+        info: "Hamburguesa, con la mejor carne, Precio: $60",
+        rotation: "0 180 0"
+      },
+      {
+        url:
+          "https://cdn.glitch.com/05e1a300-5fe8-4c3d-aa8e-bc069bcb2db8%2FPizza.glb?v=1623293471899",
+        scale: "30 30 30",
+        rotation: "0 180 0",
+        info: "Pizza, Hawallana relleno de queso, Precio: $20"
+      },
+      {
+        url:
+          "https://cdn.glitch.com/05e1a300-5fe8-4c3d-aa8e-bc069bcb2db8%2Fcombo.glb?v=1623293390010",
+        scale: "3 3 3",
+        rotation: "0 180 0",
+        info: "Combo de Hamburguesa, incluye refresco, Precio: $120"
+      }
     ];
 
     var modelIndex = 0;
-    var setModel = function (model, entity) {
-        if (model.scale) {
-            entity.setAttribute('scale', model.scale);
-        }
+    var setModel = function(model, entity) {
+      if (model.scale) {
+        entity.setAttribute("scale", model.scale);
+      }
 
-        if (model.rotation) {
-            entity.setAttribute('rotation', model.rotation);
-        }
+      if (model.rotation) {
+        entity.setAttribute("rotation", model.rotation);
+      }
 
-        if (model.position) {
-            entity.setAttribute('position', model.position);
-        }
+      if (model.position) {
+        entity.setAttribute("position", model.position);
+      }
 
-        entity.setAttribute('gltf-model', model.url);
+      entity.setAttribute("gltf-model", model.url);
 
-        const div = document.querySelector('.instructions');
-        div.innerText = model.info;
+      const div = document.querySelector(".instructions");
+      div.innerText = model.info;
     };
 
     function renderPlaces(places) {
-        let scene = document.querySelector('a-scene');
+      let scene = document.querySelector("a-scene");
 
-        places.forEach((place) => {
-            let latitude = place.location.lat;
-            let longitude = place.location.lng;
+      places.forEach(place => {
+        let latitude = place.location.lat;
+        let longitude = place.location.lng;
 
-            let model = document.createElement('a-entity');
-            model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
+        let model = document.createElement("a-entity");
+        model.setAttribute(
+          "gps-entity-place",
+          `latitude: ${latitude}; longitude: ${longitude};`
+        );
 
-            setModel(models[modelIndex], model);
+        setModel(models[modelIndex], model);
 
-            model.setAttribute('animation-mixer', '');
+        model.setAttribute("animation-mixer", "");
 
-            document.querySelector('button[data-action="change"]').addEventListener('click', function () {
-                var entity = document.querySelector('[gps-entity-place]');
-                modelIndex++;
-                var newIndex = modelIndex % models.length;
-                setModel(models[newIndex], entity);
-            });
+        document
+          .querySelector('button[data-action="change"]')
+          .addEventListener("click", function() {
+            var entity = document.querySelector("[gps-entity-place]");
+            modelIndex++;
+            var newIndex = modelIndex % models.length;
+            setModel(models[newIndex], entity);
+          });
 
-            scene.appendChild(model);
-        });
+        scene.appendChild(model);
+      });
     }
